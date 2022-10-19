5573caca2ae28df775b8763e40a78d8f594ec29b
Roberto Cardenas <robertocardenas@berkeley.edu>
2022-10-19 00:56:20 -0700

    Flask mail attempt one

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_mail import Mail, Message
 
 from home import home_bp
 from projects import projects_bp
-from contact import contact_bp
 from about import about_bp
 
 app = Flask(__name__)
 
+app.config['MAIL_SERVER'] = 'smtp.elasticemail.com'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = 'noreply@robertocardenas.dev'
+app.config['MAIL_PASSWORD'] = 'D2890969BAE3517CC4FD158AACDB7C8A038D'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
+
 app.register_blueprint(home_bp)
 app.register_blueprint(projects_bp, url_prefix="/projects")
-app.register_blueprint(contact_bp, url_prefix="/contact")
 app.register_blueprint(about_bp, url_prefix="/about")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  if request.method == 'POST':
+    msg = Message("Hey", sender='noreply@demo.com', recipients=['robertocardenas@berkeley.edu'])
+    msg.body = "This is an email message"
+    mail.send(msg)
+
+  return render_template("contact.html")
--- contact.py
+++ contact.py
@@ -1,9 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, current_app
+from flask_mail import Mail, Message
 
 contact_bp = Blueprint('contact_bp', __name__, 
                     template_folder='templates', 
                     static_folder='static')
 
-@contact_bp.route('/')
-def index():
-  return render_template("contact.html")
