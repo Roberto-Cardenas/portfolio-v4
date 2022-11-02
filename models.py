dd30a7c51946d1e010eb14f2a7ca2629434c7077
Roberto Cardenas <robertocardenas@berkeley.edu>
2022-11-02 15:56:13 -0700

    Made all pictures JPG format for the web, hopefully cut down on loading times

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Projects():
           'description': 'The intention of this website is to allow the Waste Reduction Manager of the unit to provide an accesible and centralized way for members of the cooperative to find the relevant information when it comes to sorting waste properly in the 124 person unit. The WRM can easily update this information by using the companion administrator page.',
           'description2': 'The client is built utilizing ReactJS and is fully responsive. The website consumes its data from a simple API written in PHP.',
           'languageIcons': ['reactjs.svg', 'html.svg', 'css.svg'],
-          'thumbnail': 'goToSiteBtn.png',
+          'thumbnail': 'goToSiteBtn.jpg',
           'github': 'https://github.com/Roberto-Cardenas/cz-waste',
           'website': 'https://cz-waste.herokuapp.com/'
         },
@@ -21,7 +21,7 @@ class Projects():
           'description': 'The intention of this website is to allow the Waste Reduction Manager of the unit to provide an accesible and centralized way for members of the cooperative to find the relevant information when it comes to sorting waste properly in the 124 person unit.',
           'description2': 'The client is built utilizing ReactJS and is fully responsive. The website consumes and writes its data from and to a simple API written in PHP. ',
           'languageIcons': ['reactjs.svg', 'html.svg', 'css.svg'],
-          'thumbnail': 'goToSiteBtn.png',
+          'thumbnail': 'goToSiteBtn.jpg',
           'github': 'https://github.com/Roberto-Cardenas/cz-waste-admin',
           'website': 'https://cz-waste-admin.herokuapp.com/'
         },
@@ -31,20 +31,10 @@ class Projects():
           'description': 'Simple REST API that serves as the backend for both the Casa Zimbabwe Waste Website and its companion Administrator page. Documentation on the API can be found by following the Github link.',
           'description2': 'The API itself is written in PHP and the data is stored in JSON files.',
           'languageIcons': ['php.svg'],
-          'thumbnail': 'goToSiteBtn.png',
+          'thumbnail': 'goToSiteBtn.jpg',
           'github': 'https://github.com/Roberto-Cardenas/cz-waste-api',
           'website': 'https://cz-waste-api.herokuapp.com/'
-        },
-        {
-          'title': 'Roberto Cardenas Web Portfolio',
-          'shortDescription': 'Web portfolio designed and implemented by me that you are currently using to check out my work!',
-          'description': '',
-          'description2': '',
-          'languageIcons': ['flask.svg', 'html.svg', 'css.svg'],
-          'thumbnail': 'homeSplash.png',
-          'github': '',
-          'website': ''
-        },
+        }
       ]
     }
 
