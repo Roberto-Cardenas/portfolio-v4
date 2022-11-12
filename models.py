57fa3a42251fbbdae90c4e5ad15f69952a5fbd3b
Roberto Cardenas <robertocardenas@berkeley.edu>
2022-11-11 22:53:31 -0800

    Redesign of individual project page, updated project model to reflect that

--- models.py
+++ models.py
@@ -8,32 +8,41 @@ class Projects():
         {
           'title': 'CZ Waste Website',
           'shortDescription': 'Waste Information Website for Casa Zimbabwe, a student housing unit that is part of the Berkeley Student Cooperative.',
-          'description': 'The intention of this website is to allow the Waste Reduction Manager of the unit to provide an accesible and centralized way for members of the cooperative to find the relevant information when it comes to sorting waste properly in the 124 person unit. The WRM can easily update this information by using the companion administrator page.',
-          'description2': 'The client is built utilizing ReactJS and is fully responsive. The website consumes its data from a simple API written in PHP.',
-          'languageIcons': ['reactjs.svg', 'html.svg', 'css.svg'],
-          'thumbnail': 'goToSiteBtn.jpg',
-          'github': 'https://github.com/Roberto-Cardenas/cz-waste',
-          'website': 'https://cz-waste.herokuapp.com/'
-        },
-        {
-          'title': 'CZ Waste Website Admin',
-          'shortDescription': 'Administrator website for the Casa Zimbabwe Waste Information Website. Casa Zimbabwe is a student housing unit that is part of the Berkeley Student Cooperative.',
-          'description': 'The intention of this website is to allow the Waste Reduction Manager of the unit to provide an accesible and centralized way for members of the cooperative to find the relevant information when it comes to sorting waste properly in the 124 person unit.',
-          'description2': 'The client is built utilizing ReactJS and is fully responsive. The website consumes and writes its data from and to a simple API written in PHP. ',
-          'languageIcons': ['reactjs.svg', 'html.svg', 'css.svg'],
-          'thumbnail': 'goToSiteBtn.jpg',
-          'github': 'https://github.com/Roberto-Cardenas/cz-waste-admin',
-          'website': 'https://cz-waste-admin.herokuapp.com/'
-        },
-        {
-          'title': 'CZ Waste REST API',
-          'shortDescription': 'REST API for the Casa Zimbabwe Waste Information Website and its companion Administrator tool. Casa Zimbabwe is a student housing unit that is part of the Berkeley Student Cooperative.',
-          'description': 'Simple REST API that serves as the backend for both the Casa Zimbabwe Waste Website and its companion Administrator page. Documentation on the API can be found by following the Github link.',
-          'description2': 'The API itself is written in PHP and the data is stored in JSON files.',
-          'languageIcons': ['php.svg'],
-          'thumbnail': 'goToSiteBtn.jpg',
-          'github': 'https://github.com/Roberto-Cardenas/cz-waste-api',
-          'website': 'https://cz-waste-api.herokuapp.com/'
+          'languageIcons': ['reactjs.svg', 'html.svg', 'css.svg', 'php.svg'],
+          'thumbnail': {
+            'filename': 'wasteSplash.jpg',
+            'title': 'Casa Zimbabwe',
+            'alt': 'The courtyard of Casa Zimbabwe'
+          },
+          'description': 'The unit houses 124 students and produces a massive amount of waste every year. As the former Waste Reduction Manager, I created this tool to have an accesible way of educating the residents on proper waste sorting practices as well as having an easy to manage educational resource for future WRM’s.',
+          'technology': 'Both the user and admin clients are built in ReactJS and are fully responsive. The data is stored in a REST API written in PHP.',
+          'features': [
+              'User client with color coded panels representing each type of waste.',
+              'Admin client for managing content. Panels can be created, deleted, edited, as well as reordered. Same goes for the content within each panel.',
+              'Data stored in fully documented REST API.'
+          ],
+          'contributions': [
+              'Drew initial sketches and final designs in Figma',
+              'Built HTML templates based on designs',
+              'Utilized ReactJS to build both the user and admin clients',
+              'Designed and built the REST API from scratch in PHP',
+              'Tested REST API using Postman',
+              'Setup hosting for the site on the local server within the housing unit'
+          ],
+          'image1': {
+            'filename': 'wasteUserDash.jpg',
+            'title': 'User Dashboard',
+            'alt': 'Screenshot of user dashboard'
+          },
+          'image2': {
+            'filename': 'wasteAdminDash.jpg',
+            'title': 'Admin Dashboard',
+            'alt': 'Screenshot of adminitrator dashboard'
+          },
+          'links': [
+            {'title': 'Go to website', 'link': 'https://cz-waste.herokuapp.com/'},
+            {'title': 'Checkout the code', 'link': 'https://github.com/Roberto-Cardenas/cz-waste'}
+          ]
         }
       ]
     }
